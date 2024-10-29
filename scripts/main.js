fc485bfe8eeeea94d1f4b2ddf54de74b6f6dd41e
Kiara Cherry <kiaracherry2006@gmail.com>
2024-10-29 12:54:13 -0700

    some changes

--- scripts/main.js
+++ scripts/main.js
@@ -60,58 +60,90 @@ function readQuote(day) {
         });
 }
 
-function writeSpots() {
+function writeHikes() {
     //define a variable for the collection you want to create in Firestore to populate data
-    var spotsRef = db.collection("Study Spots");
+    var hikesRef = db.collection("hikes");
 
-    spotsRef.add({
-        code: "CafVan04",
-        name: "Cafe", //replace with your own city?
-        city: "Vancouver",
+    hikesRef.add({
+        code: "BBY01",
+        name: "Burnaby Lake Park Trail", //replace with your own city?
+        city: "Burnaby",
         province: "BC",
-        wifi: "yes",
-        outlets: "yes",
-        washrooms: "yes",
-        details: "Quiet ambiance, friendly staff, great place to study!",
-        open: "9:00am",      //number value
-        close: "10:00pm",     //number value
+        level: "easy",
+        details: "A lovely place for lunch walk",
+        length: 10,          //number value
+        hike_time: 60,       //number value
         lat: 49.2467097082573,
         lng: -122.9187029619698,
         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
     });
-    spotsRef.add({
-        code: "LiS01",
-        name: "Semiahmoo Library", //replace with your own city?
-        city: "Surrey",
+    hikesRef.add({
+        code: "AM01",
+        name: "Buntzen Lake Trail", //replace with your own city?
+        city: "Anmore",
         province: "BC",
-        wifi: "yes",
-        outlets: "yes",
-        washrooms: "yes",
-        details: "Quiet rooms, chill place to study",
-        open: "9:30am",      //number value
-        close: "9:00pm",     //number value
+        level: "moderate",
+        details: "Close to town, and relaxing",
+        length: 10.5,      //number value
+        hike_time: 80,     //number value
         lat: 49.3399431028579,
         lng: -122.85908496766939,
         last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
     });
-    spotsRef.add({
-        code: "OtWR03",
-        name: "Park", //replace with your own city?
-        city: "White ROck",
+    hikesRef.add({
+        code: "NV01",
+        name: "Mount Seymour Trail", //replace with your own city?
+        city: "North Vancouver",
         province: "BC",
-        wifi: "no",
-        outlets: "no",
-        washrooms: "yes",
-        details: "Great place to get fresh air and study!",
-        open: "sunrise",      //number value
-        close: "sunset",     //number value
+        level: "hard",
+        details: "Amazing ski slope views",
+        length: 8.2,        //number value
+        hike_time: 120,     //number value
         lat: 49.38847101455571,
         lng: -122.94092543551031,
         last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2023"))
     });
 }
 
-writeSpots();
+//------------------------------------------------------------------------------
+// Input parameter is a string representing the collection we are reading from
+//------------------------------------------------------------------------------
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("hikeCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection).get()   //the collection called "hikes"
+        .then(allHikes => {
+            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            allHikes.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;       // get value of the "name" key
+                var details = doc.data().details;  // get value of the "details" key
+                var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+                var hikeLength = doc.data().length; //gets the length field
+                var docID = doc.id;
+                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-length').innerHTML = hikeLength + "km";
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
+
+                //Optional: give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery, Example: "hikes-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}
+
+displayCardsDynamically("hikes");  //input param is the name of the collection
+
 insertNameFromFirestore();
 readQuote("tuesday");        //calling the function
 getNameFromAuth(); //run the function
