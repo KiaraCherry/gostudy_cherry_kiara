b61f8039eb89b3f391620f76e7199702dec71fb6
Kiara Cherry <kiaracherry2006@gmail.com>
2024-11-26 10:34:11 -0800

    Adjust and add bookmark function

--- scripts/main.js
+++ scripts/main.js
@@ -150,20 +150,30 @@ function displayCardsDynamically(collection) {
 // It adds the hike to the "bookmarks" array
 // Then it will change the bookmark icon from the hollow to the solid version. 
 function saveBookmark(hikeDocID) {
-    // Manage the backend process to store the hikeDocID in the database, recording which hike was bookmarked by the user.
-    currentUser.update({
-        // Use 'arrayUnion' to add the new bookmark ID to the 'bookmarks' array.
-        // This method ensures that the ID is added only if it's not already present, preventing duplicates.
-        bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeDocID)
-    })
-        // Handle the front-end update to change the icon, providing visual feedback to the user that it has been clicked.
-        .then(function () {
-            console.log("bookmark has been saved for" + hikeDocID);
-            let iconID = 'save-' + hikeDocID;
-            //console.log(iconID);
-            //this is to change the icon of the hike that was saved to "filled"
-            document.getElementById(iconID).innerText = 'bookmark';
-        });
+    currentUser.get().then((userDoc) => {
+        // Manage the backend process to store the hikeDocI
+        const bookmarks = userDoc.data().bookmarks;
+        if (bookmarks.includes(hikeDocID)) {
+            currentUser.update({
+                // Use 'arrayUnion' to add the new bookmark ID to the 'bookmarks' array.
+                // This method ensures that the ID is added only if it's not already present, preventing duplicates.
+                bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeDocID)
+            })
+        } else {
+            currentUser
+                .update({
+                    bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeDocID),
+                })
+                // Handle the front-end update to change the icon, providing visual feedback to the user that it has been clicked.
+                .then(function () {
+                    console.log("bookmark has been saved for" + hikeDocID);
+                    let iconID = 'save-' + hikeDocID;
+                    //console.log(iconID);
+                    //this is to change the icon of the hike that was saved to "filled"
+                    document.getElementById(iconID).innerText = 'bookmark';
+                });
+        }
+    });
 }
 
 
